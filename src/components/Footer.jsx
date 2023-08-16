a46ec346540b52ee3b7b96c4c3c1496613352523
khaledmasry0 <khaledmohamed3360@gmail.com>
2023-08-16 19:35:13 +0300

    publish the app

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <span>KHaled Mohamed</span>
+      <span>khaledmohamed3360@gmail.com</span>
+    </footer>
+  );
+};
+
+export default Footer;
